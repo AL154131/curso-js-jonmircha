b0c7e65edc80d19aee3955dfc225610565e2ff13
Dan1an <garayrodrigueziandaniel@gmail.com>
2021-10-02 16:16:06 -0600

    Clase #50 "Introduccion al DOM"

--- 49_web_apis.js
+++ 49_web_apis.js
@@ -7,4 +7,6 @@ const hablar = (texto) => {
   speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
 };
 
-hablar(texto);
+// hablar(texto);
+
+document.write('<h1>WEB APIs</h1><ul><li>Eventos</li><li>Forms</li><li>AJAX - Fetch</li><li>History</li><li>Web Storage</li><li>Geolocation</li><li>Drag & Drop</li><li>Indexed DB</li><li>Canvas</li><li>MatchMedia</li><li>etc...</li></ul><h2>DOM:Document Object Model</h2><h2>CSSOM: CSS Object Model</h2><h2>BOM: Browser Object Model</h2>');
